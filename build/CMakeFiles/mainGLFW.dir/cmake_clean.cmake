2815ef4504dc23c2c513ad4209298dd4896e932b
Kade Johnston <kademjohnston@gmail.com>
2026-04-08 12:51:29 -0600

    Test

---
+++ build/CMakeFiles/mainGLFW.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mainGLFW.dir/link.d"
+  "CMakeFiles/mainGLFW.dir/src/main.c.o"
+  "CMakeFiles/mainGLFW.dir/src/main.c.o.d"
+  "mainGLFW"
+  "mainGLFW.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mainGLFW.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
